ec7bf7a2df6defae9124fcb815c1f32e89b7a435
Garl <gjnorm1@gmail.com>
2023-02-06 11:08:12 -0600

    add games page

---
+++ ClientApp/src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import './Button.css'
+
+export const Button = ({href, text, onClick}) => {
+    return (
+        <button
+            className="gamebutton"
+            href={href ? href : null}
+            onClick={onClick}>
+            {text}
+        </button>
+        )
+}
